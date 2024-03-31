226da993034b1de4ebe907476624cfd251f887a3
Arjun kumar vs <arjun.tech177@gmail.com>
2024-04-01 04:25:42 +0530

    css configurations done for dark and light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,28 @@ import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage/HomePage";
 import LoginPage from "./scenes/loginPage/LoginPage";
 import ProfilePage from "./scenes/profilePage/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+import { themeSettings } from "./theme";
 
 function App() {
+
+  const mode  = useSelector((state)=> state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
+      <ThemeProvider theme={theme}>
+        {/* <CssBaseline /> */}
+      <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
+      </ThemeProvider>
       </BrowserRouter>
     </div>
   );
