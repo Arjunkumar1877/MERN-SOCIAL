314f4dcf8bed6fbee536e53c0352a155f8079e84
Arjun kumar vs <arjun.tech177@gmail.com>
2024-04-01 05:29:41 +0530

    Navbar created with material ui library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,31 @@
 import "./App.css";
-import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage/HomePage";
 import LoginPage from "./scenes/loginPage/LoginPage";
 import ProfilePage from "./scenes/profilePage/ProfilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/system";
+import {  ThemeProvider } from "@mui/material/styles"; // Update import
+import { createTheme } from "@mui/material/styles"; // Update import
 import { themeSettings } from "./theme";
+import Navbar from "./scenes/navbar/Navbar";
 
 function App() {
-
-  const mode  = useSelector((state)=> state.mode);
-  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        {/* <CssBaseline /> */}
-      <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          {/* <CssBaseline /> */}
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
